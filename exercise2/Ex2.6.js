15f40fbd744d52fba5e2f6a340712988c1d1b8a5
Nita1001 <devinedevops@gmail.com>
2023-01-19 18:59:34 +0200

    halfway through

--- exercise2/Ex2.6.js
+++ exercise2/Ex2.6.js
@@ -9,3 +9,22 @@
 // centuryFromYear( 1900) returns (19)
 // centuryFromYear(1601) returns (17)
 // centuryFromYear(2000) returns (20)
+const centuryCheck = (year) => {
+    let digit = year / 1000;
+    let key = 0;
+
+    if(digit >= 0 && digit < 1){
+        //centuries range (1 - 10)
+        console.log('centuries range (1 - 10)');
+
+    } else {
+        //centuries range (11 - 20)
+        //centuries range (21 - 30)
+            console.log('centuries range (11 - 20)');
+            let dig = year / 10;
+            //(year % 10 >= 1 || year % 10 <=9 ) ?   
+       
+    }
+    console.log(key);
+}
+centuryCheck(1001);
