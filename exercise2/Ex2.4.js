9525b4fe96ca5bd957e369e2694e828b9adb1336
Nita1001 <devinedevops@gmail.com>
2023-01-19 16:40:27 +0200

    done

--- exercise2/Ex2.4.js
+++ exercise2/Ex2.4.js
@@ -8,8 +8,7 @@ let arr = [0, 0, 0.55, 0, 0];
 const fundUniq = (arr) => {
     arr.sort();
     let arrd = 0;
-    if((arr[0] === arr[1]) && (arr[arr.length - 1] !== arr[arr.length - 2]))
-    {
+    if((arr[0] === arr[1]) && (arr[arr.length - 1] !== arr[arr.length - 2])){
         arrd = arr[arr.length -1];
     } else if((arr[0] !== arr[1]) && (arr[arr.length - 1] === arr[arr.length - 2])){
         arrd = arr[0];
