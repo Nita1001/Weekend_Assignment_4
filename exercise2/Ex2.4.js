5e9a997905e04f069d7ab954d878388fa064e3bb
Nita1001 <devinedevops@gmail.com>
2023-01-19 15:37:58 +0200

    Initial commit

---
+++ exercise2/Ex2.4.js
@@ -0,0 +1,6 @@
+// Ex2.4 - Unique
+
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
