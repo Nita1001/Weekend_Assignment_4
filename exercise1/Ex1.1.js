c697c1d2c2d83ff3adcaf84e4e3cd254a0f4c18c
Nita1001 <devinedevops@gmail.com>
2023-01-19 14:37:09 +0200

    done

--- exercise1/Ex1.1.js
+++ exercise1/Ex1.1.js
@@ -1,3 +1,7 @@
+// Ex1.1 - Yes or No
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No"
+// string for false.
 const yesOrNo = (bool) => (bool) ? 'Yes' : 'No';
 let answer = true;
 console.log(yesOrNo(answer));
