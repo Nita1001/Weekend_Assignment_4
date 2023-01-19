347be42e8bf3c399ce85f1edad34ac7b92a61ece
Nita1001 <devinedevops@gmail.com>
2023-01-19 19:06:52 +0200

    done

--- exercise2/Ex2.7.js
+++ exercise2/Ex2.7.js
@@ -9,3 +9,27 @@
 // basicOp('-', 15, 18) // Output: -3
 // basicOp('*', 5, 5) // Output: 25
 // basicOp('/', 49, 7) // Output: 7
+const calcThis = (operator, num1, num2) => {
+    let sum = 0;
+    switch(operator){
+        case '+':
+            sum = num1 + num2;
+            break;
+        case '-':
+            sum = num1 - num2;
+            break;
+        case '*':
+            sum = num1 * num2;
+            break;
+        case '/':
+            sum = num1 / num2;
+            break;
+        default: 
+            console.log('something went wrong');        
+    }
+    return sum;
+}
+const num1 = 15;
+const num2 = 18;
+const operator = '-';
+console.log(calcThis(operator, num1, num2));
