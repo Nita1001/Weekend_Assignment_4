d6907515643b170efd8a60dbc7d6e4e49cec731e
Nita1001 <devinedevops@gmail.com>
2023-01-19 16:39:57 +0200

    done

--- exercise2/Ex2.4.js
+++ exercise2/Ex2.4.js
@@ -4,3 +4,17 @@
 // findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
 // findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
 // It’s guaranteed that array contains at least 3 numbers.
+let arr = [0, 0, 0.55, 0, 0];
+const fundUniq = (arr) => {
+    arr.sort();
+    let arrd = 0;
+    if((arr[0] === arr[1]) && (arr[arr.length - 1] !== arr[arr.length - 2]))
+    {
+        arrd = arr[arr.length -1];
+    } else if((arr[0] !== arr[1]) && (arr[arr.length - 1] === arr[arr.length - 2])){
+        arrd = arr[0];
+    }
+    console.log(arrd);
+    return arr;
+}
+fundUniq(arr);
