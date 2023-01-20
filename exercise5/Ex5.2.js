8c4d1c33adf40980d96c4f5e92ce33cd0412b07e
Nita1001 <devinedevops@gmail.com>
2023-01-20 17:11:36 +0200

    done

--- exercise5/Ex5.2.js
+++ exercise5/Ex5.2.js
@@ -3,3 +3,12 @@
 // Write a function called repeat_str which repeats the given string src exactly count times.
 // repeatStr(6, "I") // "IIIIII"
 // repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+const repeatStr = (count, str) => {
+    let newStr = str;
+    while(count > 1){
+        newStr = newStr.concat(str);
+        count--;
+    }
+    return newStr;
+}
+console.log(repeatStr(8, 'Hey'));
