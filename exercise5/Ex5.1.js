b7eef493cd7222c7846a91d7d36978527482ec82
Nita1001 <devinedevops@gmail.com>
2023-01-20 17:04:11 +0200

    done

--- exercise5/Ex5.1.js
+++ exercise5/Ex5.1.js
@@ -3,3 +3,11 @@
 // It's pretty straightforward. Your goal is to create a function that removes the first and last
 // characters of a string. You're given one parameter, the original string. You don't have to worry
 // with strings with less than two characters.
+let str = "Hey, can you trim me? thanks";
+const trim = (str) => {
+    let newStr = str.split('');
+    newStr.pop();
+    newStr.shift();
+    return newStr.join('');
+}
+console.log(trim(str));
