a68420d5566b10154cbd38a64a9115ec203dee42
Nita1001 <devinedevops@gmail.com>
2023-01-20 16:11:23 +0200

    done

--- exercise4/Ex4.1.js
+++ exercise4/Ex4.1.js
@@ -1,10 +1,6 @@
 // Ex4.1 - Fibonacci -
 
-// “Write a function to return an n element in Fibonacci sequence” is one of the most common
-// questions you can hear during the coding challenge interview part. In this blogpost I’m going to
-// walk through the two of the most typical solutions for this problem and also cover a dreadful (for
-// most of novice developers) topic of time complexity.
-// So what is a Fibonacci sequence? According to Wikipedia:
+// ?? “Write a function to return an n element in Fibonacci sequence” 
 // “In mathematics, the Fibonacci numbers are the numbers in the following integer sequence,
 // called the Fibonacci sequence, and characterized by the fact that every number after the first
 // two is the sum of the two preceding ones.”
@@ -13,3 +9,12 @@
 // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …
 // or this:
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …
+const fibonacci = (n) => {
+    let arr = [0, 1];
+    for(let i = 2; i <= n; i++)
+    {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    } 
+    return arr[n];
+}
+console.log(fibonacci(15));
