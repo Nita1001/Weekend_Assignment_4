bf178903aa30ed6b7d4ab4ea7c4906a8c373dabb
Nita1001 <devinedevops@gmail.com>
2023-01-21 12:12:46 +0200

    done

--- exercise5/Ex5.8.js
+++ exercise5/Ex5.8.js
@@ -2,3 +2,14 @@
 
 // Given a string of words, return the longest word[s].
 // String will never be empty and you do not need to account for different data types.
+let str = "shortest word longerWord theLongestWord";
+const shortest = (str) => {
+    let newStr = str.split(' ');
+    let lengths = [];
+    newStr.forEach((element) => lengths.push(element.length));
+    let lengthOfLongestWord = Math.max(...lengths);
+    let index = lengths.indexOf(lengthOfLongestWord);
+    let word = newStr[index];
+    return word;
+}
+console.log(shortest(str));
