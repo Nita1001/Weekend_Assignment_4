d1fa1ac3a8af0f36239ff996796b48f6eb554f99
Nita1001 <devinedevops@gmail.com>
2023-01-21 12:10:23 +0200

    done

--- exercise5/Ex5.7.js
+++ exercise5/Ex5.7.js
@@ -2,3 +2,14 @@
 
 // Simple, given a string of words, return the length of the shortest word(s).
 // String will never be empty and you do not need to account for different data types.
+let str = "shortest hi longerWord theLongestWord";
+const shortest = (str) => {
+    let newStr = str.split(' ');
+    let lengths = [];
+    newStr.forEach((element) => lengths.push(element.length));
+    let lengthOfShortestWord = Math.min(...lengths);
+    let index = lengths.indexOf(lengthOfShortestWord);
+    let word = newStr[index];
+    return word;
+}
+console.log(shortest(str));
