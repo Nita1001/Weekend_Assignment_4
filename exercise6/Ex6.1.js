d9e62675e3036fd44de576465010efeb8e265cfa
Nita1001 <devinedevops@gmail.com>
2023-01-21 14:34:49 +0200

    done

--- exercise6/Ex6.1.js
+++ exercise6/Ex6.1.js
@@ -4,6 +4,19 @@
 // accum:
 // Examples:
 // accum("abcd") -> "A-Bb-Ccc-Dddd"
-// accum("RqaEzty") -> "R-Qq-Aaa-Eeeezzzz-Tttttt-Yyyyyyy"
-// accum("cwAt") -> "C-VWw-Aaa-Tttt"
-// The parameter of accum is a string which includes only letters from a..z anda. .z.
+// accum("RqaEzty") -> "R-Qq-Aaa-zzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z
+let str = 'RqaEzty';
+const accum = (str) => {
+    let newStr = str.split('');
+    for(let i = 0; i < str.length ;i++){
+        newStr[i] = str[i].repeat(i + 1); 
+    }
+    newStr = newStr.map((element, index) => {
+      return str[index].toUpperCase() + element[index].repeat(index);
+    });
+    newStr = newStr.join('-');
+    return newStr;
+}
+console.log(accum(str));
