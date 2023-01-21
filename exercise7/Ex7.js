99eca8fa67be731a67dfa7b020eac039b50cee9e
Nita1001 <devinedevops@gmail.com>
2023-01-21 18:10:54 +0200

    Initial commit

---
+++ exercise7/Ex7.js
@@ -0,0 +1,7 @@
+// Ex7 - Implement The Following JS Methods -
+
+// Implement the following methods -
+// - Filter
+// - ForEach
+// - Map
+// Using only for(), array and objects (without other js methods)
---
+++ exercise8/Ex8.js
@@ -0,0 +1,12 @@
+// Ex 8 - Find the Perimeter of a Rectangle
+
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+// Examples
+// f£ 7 26
+// indPerimeter(6, 7)
+// findPerimeter(20, 10) —™60
+// findPerimeter(2, 9) —™22
+// Notes
+// e Don't forget to return the result.
+// e If you're stuck, find help in the Resources tab.
+// e If you're really stuck, find solutions in the Solutions tab.
